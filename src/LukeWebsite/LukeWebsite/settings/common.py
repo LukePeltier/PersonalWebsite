a494cca4514154c8922e1a4663ddc99695f90ed4
Luke Peltier <luke@lukepeltier.com>
2022-08-05 16:29:35 -0500

    Move templates to one place, make npm project

--- src/LukeWebsite/LukeWebsite/settings/common.py
+++ src/LukeWebsite/LukeWebsite/settings/common.py
@@ -58,8 +58,8 @@ ROOT_URLCONF = 'LukeWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
